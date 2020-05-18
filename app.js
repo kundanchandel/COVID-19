e0cc083ee0d99d535b63322205c70e33dd693e35
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-05-18 14:20:18 +0530

    Make table and show stats using api

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var fetch   = require("node-fetch")
+var app     = express()
+
+app.get("/", async function(req, res){
+    fetch("https://api.rootnet.in/covid19-in/stats/latest")
+    .then(res => res.json())
+    .then((data) => {
+        // do something with JSON
+        res.render("index.ejs", {data: data})
+        console.log(data)
+    });
+})
+
+
+
+app.listen(7000, function(){
+    console.log("Server is running...")
+})
